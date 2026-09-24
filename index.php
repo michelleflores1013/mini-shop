81bb53584af61e2dd503a8069e9da598aac58203
michelleflores1013 <floresmichelle1013@gmail.com>
2026-09-24 19:21:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if (file_exists(__DIR__ . '/app/controllers/HomeController.php')) {
     exit;
 }
 
-require_once __DIR__ . '/header.php';
+require_once __DIR__ . '/includes/header.php';
 require_once __DIR__ . '/db.php';
 
 $stmt = $pdo->query("SELECT * FROM products ORDER BY created_at DESC LIMIT 12");
@@ -39,4 +39,4 @@ $products = $stmt->fetchAll();
     </div>
 </div>
 
-<?php require_once __DIR__ . '/footer.php'; ?>
+<?php require_once __DIR__ . '/includes/footer.php'; ?>
